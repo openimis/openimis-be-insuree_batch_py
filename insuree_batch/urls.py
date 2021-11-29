6b16427693899efdf03d3ebd6e955ff6dccea80a
Eric Darchis <darchis@pobox.com>
2021-11-29 22:14:52 +0100

    Add html view with QR codes (wip)
    The view is setup but the presentation is not good yet.
    `http://localhost:8000/api/insuree_batch/batch_qr/?batch=db6e3d97-bcc9-4643-9bb4-a6895a5c05bc`

--- insuree_batch/urls.py
+++ insuree_batch/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('exportInsurees/', views.export_insurees, name='export_insurees')
+    path('exportInsurees/', views.export_insurees, name='export_insurees'),
+    path('batch_qr/', views.batch_qr, name='batch_qr'),
 ]
