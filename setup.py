1b73781ec559f399de2c98b962da278a7b4bbeeb
Eric Darchis <darchis@pobox.com>
2022-05-20 15:50:14 +0200

    v1.4.0 (correct)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="openimis-be-insuree_batch",
-    version="1.3.1",
+    version="1.4.0",
     packages=find_packages(),
     include_package_data=True,
     license="GNU AGPL v3",
@@ -29,7 +29,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
